d161436499bba6d9bab17c8683128d133a09cf4f
piotryfon <pit.pit@wp.pl>
2017-11-19 12:36:16 +0100

    insertPlayer

--- game/js/scoreboard.js
+++ game/js/scoreboard.js
@@ -1,17 +1,36 @@
 var scoreboard = [];
 
+function PlayerClass(_name, _score){
+    this.name = _name;
+    this.score = _score;
+}
+
+
 function initScoreboard() {
+    // noinspection JSAnnotator
+    var emptyPlayer = new PlayerClass("",0);
 
+    for (var i=0; i<5; i++){
+        scoreboard[i] = emptyPlayer;
+    }
 };
 
-function createPlayer() {
-
+function createPlayer(name, score) {
+   var player = new PlayerClass(name, score);
+   return player;
 };
 
-function insertPlayer() {
+function insertPlayer(player) {
+
+    for (var i=0; i<5; i++){
+        if (player.score >= scoreboard[i].score){
+            scoreboard.splice(i, 0, player);
+            scoreboard.pop();
+        }
+    }
 
 };
 
 function getResults() {
-
+  return scoreboard;
 };
