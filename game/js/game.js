7d821dd5fb013974cb92dd74780ebad4a35dcf47
ilonakondrat <ilona.kondrat@gmail.com>
2017-11-26 14:22:36 +0100

    tabela wynikow

--- game/js/game.js
+++ game/js/game.js
@@ -1,7 +1,28 @@
 $(document).ready(function () {
 
-  function initGame() {
 
-  }
 
+    initScoreboard();
+
+    let myScoreboard = getResults(scoreboard);
+
+
+    $('#myModal').on('shown.bs.modal', function () {
+        let $table = $('#modalBody');
+        $table.empty();
+        myScoreboard.forEach(function (e, i) {
+
+            $table.append(`
+    <tr>
+        <td>${i + 1}</td>
+        <td>${e.name}</td>
+        <td>${e.score}</td>
+    </tr>`);
+
+        })
+    })
 });
+
+function initGame() {
+
+}
