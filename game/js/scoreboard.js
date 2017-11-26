c7adee32c8480987128cdfa7d189d2a995665e53
piotryfon <pit.pit@wp.pl>
2017-11-26 13:01:53 +0100

    gra szkielet

--- game/js/scoreboard.js
+++ game/js/scoreboard.js
@@ -1,18 +1,12 @@
 var scoreboard = [];
 
-function PlayerClass(_name, _score){
-    this.name = _name;
-    this.score = _score;
-}
-
-
 function initScoreboard() {
     if (scoreboard.length === 0){
-        scoreboard.push(createPlayer("pit", 1000));
-        scoreboard.push(createPlayer("hsh", 800));
-        scoreboard.push(createPlayer("khy", 690));
-        scoreboard.push(createPlayer("kiy", 300));
-        scoreboard.push(createPlayer("kiny", 180));
+        scoreboard.push(createPlayer("pit", 5000));
+        scoreboard.push(createPlayer("git", 4000));
+        scoreboard.push(createPlayer("bit", 3000));
+        scoreboard.push(createPlayer("kit", 2000));
+        scoreboard.push(createPlayer("fit", 1000));
     }
 }
 
@@ -27,11 +21,11 @@ function createPlayer(name, score) {
 }
 
 function insertPlayer(player) {
-    for (var i=0; i<5; i++){
-        if (player.score >= scoreboard[i].score){
+    for (var i=0; i<5; i++) {
+        if (player.score >= scoreboard[i].score) {
             scoreboard.splice(i, 0, player);
             scoreboard.pop();
-            break
+            break;
         }
     }
 
@@ -41,5 +35,23 @@ function getResults() {
     var results = scoreboard.slice(0);
     return results;
 
+}
+
+function sample() {
+
+  initScoreboard();
+  insertPlayer(createPlayer('test', 9999));
+
+  if (getResults()[0].name === 'test' && getResults()[0].score === 9999) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+
+  if (getResults().length === 5) {
+    console.log('Tabela wyników ma długość 5');
+  } else {
+    console.log('Tabela wyników nie ma długości 5');
+  }
 
 }
