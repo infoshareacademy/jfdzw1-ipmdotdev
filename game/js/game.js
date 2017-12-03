4bb28e791bf6839ab7ac6b60dbadea862e7fc8f3
burzykovski <burzyk.mb@gmail.com>
2017-12-03 20:12:53 +0100

    game-board background v.1.0

--- game/js/game.js
+++ game/js/game.js
@@ -1,28 +1,38 @@
 $(document).ready(function () {
 
+  initScoreboard();
+
+  let myScoreboard = getResults(scoreboard);
 
 
-    initScoreboard();
+  $('#myModal').on('shown.bs.modal', function () {
+    let $table = $('#modalBody');
+    $table.empty();
+    myScoreboard.forEach(function (e, i) {
 
-    let myScoreboard = getResults(scoreboard);
-
-
-    $('#myModal').on('shown.bs.modal', function () {
-        let $table = $('#modalBody');
-        $table.empty();
-        myScoreboard.forEach(function (e, i) {
-
-            $table.append(`
+      $table.append(`
     <tr>
         <td>${i + 1}</td>
         <td>${e.name}</td>
         <td>${e.score}</td>
     </tr>`);
 
-        })
-    })
+    });
+  });
+
+  function moveBackground() {
+    var x = 0;
+    setInterval(function () {
+      x += 1;
+      $('.game-board').css('background-position', '0 ' + x + 'px');
+    }, 20);
+  }
+
+
+  function initGame() {
+    moveBackground();
+  }
+
+  initGame();
 });
 
-function initGame() {
-
-}
