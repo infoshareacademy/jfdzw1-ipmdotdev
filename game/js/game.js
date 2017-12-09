87d8b237e51bfbaf85c911adc6831a30c553e0a7
burzykovski <burzyk.mb@gmail.com>
2017-12-09 11:38:13 +0100

    obstacles v.1.0

--- game/js/game.js
+++ game/js/game.js
@@ -20,6 +20,8 @@ $(document).ready(function () {
     });
   });
 
+  const spotSize = 55; //size of one 'corridor' on game-board. 55 is 495 / 9.
+
   function moveBackground() {
     var x = 0;
     setInterval(function () {
@@ -33,25 +35,25 @@ $(document).ready(function () {
         switch(e.which) {
             case 37: // left
                 $('#stick-man').stop().animate({
-                    'marginLeft': '-=55px'
+                    'marginLeft': `-=${spotSize}px`
                 });
                 break;
 
             case 38: // up
                 $('#stick-man').stop().animate({
-                    'marginTop': '-=55px'
+                    'marginTop': `-=${spotSize}px`
                 });
                 break;
 
             case 39: // right
                 $('#stick-man').stop().animate({
-                    'marginLeft': '+=55px'
+                    'marginLeft': `+=${spotSize}px`
                 });
                 break;
 
             case 40: // down
                 $('#stick-man').stop().animate({
-                    'marginTop': '+=55px'
+                    'marginTop': `+=${spotSize}px`
                 });
                 break;
 
@@ -60,7 +62,37 @@ $(document).ready(function () {
         e.preventDefault();
     });
 
+  function Obstacle() {
+    this.x = Math.floor(Math.random() * 9) * spotSize;
+    this.width = (Math.ceil(Math.random() * 3) * spotSize);
+    this.height = (Math.ceil(Math.random() * 2) * spotSize);
+    this.top = -this.height;
+    this.$elem = $('<div class="obstacle"></div>');
+  }
 
+  Obstacle.prototype.show = function() {
+    this.$elem.css({
+      'top': `${this.top}px`, 'left': `${this.x}px`, 'width': `${this.width}px`, 'height': `${this.height}px`
+    }).appendTo($('.game-board'));
+    this.startMoving();
+  }
+
+  Obstacle.prototype.startMoving = function () {
+    let that = this;
+    setInterval(function () {
+      let newTop = parseInt(that.$elem.css('top')) + 1;
+      that.$elem.css('top', `${newTop}px`);
+
+      if (newTop >= $('.game-board').innerHeight()) {
+        that.$elem.remove();
+      }
+    }, 20);
+  }
+
+  setInterval(function () {
+    let obstacle = new Obstacle();
+    obstacle.show();
+  }, 2000);
 
   function initGame() {
     moveBackground();
