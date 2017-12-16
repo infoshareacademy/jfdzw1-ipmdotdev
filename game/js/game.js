3835766a244cbb33bfb18f2d44ca79fc15a6a0ce
burzykovski <burzyk.mb@gmail.com>
2017-12-16 21:53:09 +0100

    refactoring 1.0

--- game/js/game.js
+++ game/js/game.js
@@ -1,169 +1,109 @@
-$(document).ready(function () {
+function game() {
 
+  $(document).ready(function () {
 
-    initScoreboard();
+    const spotSize = 55; //size of one 'corridor' on game-board. 55 is 495 / 9.
+    const myScoreboard = getResults(scoreboard);
 
-    let myScoreboard = getResults(scoreboard);
+    $('#rankingModal').on('shown.bs.modal', function () {
 
+      let $table = $('#rankingModalBody');
+      $table.empty();
+      myScoreboard.forEach(function (e, i) {
 
-    $('#gameModal').on('shown.bs.modal', function () {
-
-        let $table = $('#modalBody');
-        $table.empty();
-        myScoreboard.forEach(function (e, i) {
-
-            $table.append(`
+        $table.append(`
     <tr>
         <td>${i + 1}</td>
         <td>${e.name}</td>
         <td>${e.score}</td>
     </tr>`);
 
-        });
+      });
     });
-
-    const spotSize = 55; //size of one 'corridor' on game-board. 55 is 495 / 9.
 
     function moveBackground() {
-        var x = 0;
-        setInterval(function () {
-            x += 1;
-            $('.game-board').css('background-position', '0 ' + x + 'px');
-        }, 20);
+      let x = 0;
+      setInterval(function () {
+        x += 1;
+        $('.game-board').css('background-position', '0 ' + x + 'px');
+      }, 20);
     }
 
-    let keypressed = false
-    $(document).keyup(() => keypressed = false)
-    $(document).keydown(function (e) {
-
-        var positionOfStickMan = $("#stick-man").position();
-        console.log(positionOfStickMan)
-
-        if (keypressed) {
-            return
-        }
-        keypressed = true
-        console.log('pressed', e.which)
-        switch (e.which) {
-            case 37: // left
-                if (positionOfStickMan.left === 0) {
-                    break
-                }
-                $('#stick-man').stop().animate({
-                    'left': `-=${spotSize}px`
-                }, 50);
-                break;
-
-            case 38: // up
-                if (positionOfStickMan.top < 55) {
-                    break
-                }
-                $('#stick-man').stop().animate({
-                    'top': `-=${spotSize}px`
-                }, 50);
-                break;
-
-            case 39: // right
-                if (positionOfStickMan.left > 385) {
-                    break
-                }
-                $('#stick-man').stop().animate({
-                    'left': `+=${spotSize}px`
-                }, 50);
-                break;
-
-            case 40: // down
-                if (positionOfStickMan.top > 385) {
-                    break
-                }
-                $('#stick-man').stop().animate({
-                    'top': `+=${spotSize}px`
-                }, 50);
-                break;
-
-            default:
-                return;
-        }
-        e.preventDefault();
-
-    });
-
     function Obstacle() {
-        this.x = Math.floor(Math.random() * 9) * spotSize;
-        this.width = (Math.ceil(Math.random() * 3) * spotSize);
-        this.height = (Math.ceil(Math.random() * 2) * spotSize);
-        this.top = -this.height;
-        this.$elem = $('<div class="obstacle"></div>');
+      this.x = Math.floor(Math.random() * 9) * spotSize;
+      this.width = (Math.ceil(Math.random() * 3) * spotSize);
+      this.height = (Math.ceil(Math.random() * 2) * spotSize);
+      this.top = -this.height;
+      this.$elem = $('<div class="obstacle"></div>');
     }
 
     Obstacle.prototype.show = function () {
-        this.$elem.css({
-            'top': `${this.top}px`, 'left': `${this.x}px`, 'width': `${this.width}px`, 'height': `${this.height}px`
-        }).appendTo($('.game-board'));
-        this.startMoving();
+      this.$elem.css({
+        'top': `${this.top}px`, 'left': `${this.x}px`, 'width': `${this.width}px`, 'height': `${this.height}px`
+      }).appendTo($('.game-board'));
+      this.startMoving();
     };
 
     Obstacle.prototype.startMoving = function () {
-        let that = this;
-        setInterval(function () {
+      let that = this;
+      setInterval(function () {
 
-            let newTop = parseInt(that.$elem.css('top')) + 1;
-            that.$elem.css('top', `${newTop}px`);
+        let newTop = parseInt(that.$elem.css('top')) + 1;
+        that.$elem.css('top', `${newTop}px`);
 
-            if (newTop >= $('.game-board').innerHeight()) {
-                that.$elem.remove();
-            }
-        }, 20);
+        if (newTop >= $('.game-board').innerHeight()) {
+          that.$elem.remove();
+        }
+      }, 20);
     };
 
     Obstacle.prototype.checkCollision = function (stickman) {
-        let stickmanX = parseInt(stickman.css('left').slice(0, -2));
-        let stickmanY = parseInt(stickman.css('top').slice(0, -2));
-        let obstacleX = parseInt(this.$elem.css('left').slice(0, -2));
-        let obstacleY = parseInt(this.$elem.css('top').slice(0, -2));
-        let stickmanWidth = 55;
-        let stickmanHeight = 55;
+      let stickmanX = parseInt(stickman.css('left').slice(0, -2));
+      let stickmanY = parseInt(stickman.css('top').slice(0, -2));
+      let obstacleX = parseInt(this.$elem.css('left').slice(0, -2));
+      let obstacleY = parseInt(this.$elem.css('top').slice(0, -2));
+      let stickmanWidth = 55;
+      let stickmanHeight = 55;
 
-        return obstacleX < stickmanX + stickmanWidth &&
-            obstacleX + this.width > stickmanX &&
-            obstacleY < stickmanY + stickmanHeight &&
-            this.height + obstacleY > stickmanY;
+      return obstacleX < stickmanX + stickmanWidth &&
+        obstacleX + this.width > stickmanX &&
+        obstacleY < stickmanY + stickmanHeight &&
+        this.height + obstacleY > stickmanY;
     };
 
 
     setInterval(function () {
-        let obstacle = new Obstacle();
-        obstacle.show();
+      let obstacle = new Obstacle();
+      obstacle.show();
 
-        setInterval(function () {
-            let collision = obstacle.checkCollision($('#stick-man'));
-            if (collision) {
-                // alert("Game Over")
-            }
-        }, 1000)
+      setInterval(function () {
+        let collision = obstacle.checkCollision($('#stick-man'));
+        if (collision) {
+          // alert("Game Over")
+        }
+      }, 1000)
 
     }, 2200);
 
 
-    var stickman = "#stick-man";
-
-
     function initGame() {
-        moveBackground();
-        const name = getParameterByName('name');
-        $('#playerName').html(name);
+      moveBackground();
+      const name = getParameterByName('name');
+      $('#playerName').html(name);
     }
 
     function getParameterByName(name, url) {
-        if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
-        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-            results = regex.exec(url);
-        if (!results) return null;
-        if (!results[2]) return '';
-        return decodeURIComponent(results[2].replace(/\+/g, " "));
+      if (!url) url = window.location.href;
+      name = name.replace(/[\[\]]/g, "\\$&");
+      var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return '';
+      return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
 
+    initScoreboard();
     initGame();
-});
-
+    stickmanManager({spotSize: spotSize});
+  });
+}
--- game/js/scoreboard.js
+++ game/js/scoreboard.js
@@ -1,13 +1,13 @@
 var scoreboard = [];
 
 function initScoreboard() {
-    if (scoreboard.length === 0){
-        scoreboard.push(createPlayer("pit", 5000));
-        scoreboard.push(createPlayer("git", 4000));
-        scoreboard.push(createPlayer("bit", 3000));
-        scoreboard.push(createPlayer("kit", 2000));
-        scoreboard.push(createPlayer("fit", 1000));
-    }
+  if (scoreboard.length === 0) {
+    scoreboard.push(createPlayer("pit", 5000));
+    scoreboard.push(createPlayer("git", 4000));
+    scoreboard.push(createPlayer("bit", 3000));
+    scoreboard.push(createPlayer("kit", 2000));
+    scoreboard.push(createPlayer("fit", 1000));
+  }
 }
 
 function createPlayer(name, score) {
@@ -15,38 +15,20 @@ function createPlayer(name, score) {
     name: name,
     score: score
   }
-
   return player;
 }
 
 function insertPlayer(player) {
-    for (var i=0; i<5; i++) {
-        if (player.score > scoreboard[i].score) {
-            scoreboard.splice(i, 0, player);
-            scoreboard.pop();
-            break;
-        }
+  for (var i = 0; i < 5; i++) {
+    if (player.score > scoreboard[i].score) {
+      scoreboard.splice(i, 0, player);
+      scoreboard.pop();
+      break;
     }
+  }
 }
 
 function getResults() {
-    var results = scoreboard.slice(0);
-    return results;
-}
-
-function sample() {
-  initScoreboard();
-  insertPlayer(createPlayer('test', 9999));
-
-  if (getResults()[0].name === 'test' && getResults()[0].score === 9999) {
-    console.log(true);
-  } else {
-    console.log(false);
-  }
-
-  if (getResults().length === 5) {
-    console.log('Tabela wyników ma długość 5');
-  } else {
-    console.log('Tabela wyników nie ma długości 5');
-  }
+  var results = scoreboard.slice(0);
+  return results;
 }
