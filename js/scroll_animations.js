6dd0222270f7c516f9c7452197d2359bad60c962
burzykovski <burzyk.mb@gmail.com>
2018-09-02 13:19:30 +0200

    fix invisible footer due to slideanim

--- js/scroll_animations.js
+++ js/scroll_animations.js
@@ -25,7 +25,7 @@ $(window).scroll(function() {
   $(".slideanim").each(function(){
     var pos = $(this).offset().top;
     var winTop = $(window).scrollTop();
-    if (pos < winTop + 600) {
+    if (pos < winTop + 800) {
       $(this).addClass("slide");
     }
   });
